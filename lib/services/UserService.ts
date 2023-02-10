774aac1eda3cf8a6339baf4437393215151ca15c
Gabriel Simek <gabrielsimek@outlook.com>
2023-02-10 21:47:27 +0100

    fixes: minor adjustments

--- lib/services/UserService.ts
+++ lib/services/UserService.ts
@@ -1,39 +1,26 @@
-import { compareSync, hash } from "bcrypt";
-import  { sign }  from "jsonwebtoken";
-import { PrismaClient, User } from "@prisma/client";
-import { createUserDto } from "../dtos/users.dto";
-import { UserWithoutPassword } from "../interfaces/users.interfaces";
-const prismaClient = new PrismaClient()
-
-const JWT_SECRET = typeof process.env.JWT_SECRET === 'string' ? process.env.JWT_SECRET : ""
-export default class  UserService {
-  
-  static async signUpUser({ email, password }: createUserDto): Promise<[UserWithoutPassword, string]> {
-    // Check for existing user
-    const existingUser = await prismaClient.user.findUnique({where: {email: email}})
-    console.log('existingUser', existingUser)
-    if(existingUser) throw new Error('Email already exists')
-
-
-    const passwordHash = await hash(password, Number(process.env.SALT_ROUNDS))
-
-    const user = await prismaClient.user.create({data: {email: email, password: passwordHash}})
-    console.log('user', user);
-    
-
-    const token = sign(user, JWT_SECRET, {expiresIn: '1 day'})
-    console.log('token', token);
-
-    return [removePassword(user), token]
-} 
-    
+import { hash } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import prisma from '../prisma';
+import { createUserDto } from '../dtos/users.dto';
+import createError, { HttpError } from 'http-errors';
+import { UserSelect } from '../interfaces/users.interfaces';
+const JWT_SECRET =
+  typeof process.env.JWT_SECRET === 'string' ? process.env.JWT_SECRET : '';
+export default class UserService {
+  static async signUpUser({
+    email,
+    password,
+  }: createUserDto): Promise<[UserSelect, string]> {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) throw createError(409, 'Email already exists');
+    const passwordHash = await hash(password, Number(process.env.SALT_ROUNDS));
+    const user = await prisma.user.create({
+      data: { email, password: passwordHash },
+      select: { id: true, email: true },
+    });
+    const token = sign(user, JWT_SECRET, { expiresIn: '1 day' });
+    return [user, token];
+  }
 }
-
-
-const removePassword = (user: User): UserWithoutPassword => {
-    return {
-        id: user.id,
-        email: user.email
-    }
-}
-
